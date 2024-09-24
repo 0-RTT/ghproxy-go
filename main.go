6476041e0048de519ac22dd88c839d6ec72b7036
0-RTT <60175577+0-RTT@users.noreply.github.com>
2024-09-24 17:56:02 +0800

    Update main.go
    默认缓存7天

--- main.go
+++ main.go
@@ -36,13 +36,10 @@ var (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-
 	router.GET("/", func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "https://github.com/0-RTT/ghproxy-go")
 	})
-
 	router.NoRoute(handler)
-
 	err := router.Run(fmt.Sprintf("%s:%d", host, port))
 	if err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
@@ -53,47 +50,39 @@ func handler(c *gin.Context) {
 	rawPath := strings.TrimPrefix(c.Request.URL.RequestURI(), "/")
 	re := regexp.MustCompile(`^(http:|https:)?/?/?(.*)`)
 	matches := re.FindStringSubmatch(rawPath)
-
 	rawPath = "https://" + matches[2]
-
 	matches = checkURL(rawPath)
 	if matches == nil {
 		c.String(http.StatusForbidden, "Invalid input.")
 		return
 	}
-
 	if exps[1].MatchString(rawPath) {
 		rawPath = strings.Replace(rawPath, "/blob/", "/raw/", 1)
 	}
-
 	proxy(c, rawPath)
 }
 
 func proxy(c *gin.Context, u string) {
 	req := requestPool.Get().(*http.Request)
 	defer requestPool.Put(req)
-
 	var err error
 	req, err = http.NewRequest(c.Request.Method, u, c.Request.Body)
 	if err != nil {
 		http.Error(c.Writer, fmt.Sprintf("server error %v", err), http.StatusInternalServerError)
 		return
 	}
-
 	for key, values := range c.Request.Header {
 		for _, value := range values {
 			req.Header.Add(key, value)
 		}
 	}
 	req.Header.Del("Host")
-
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		http.Error(c.Writer, fmt.Sprintf("server error %v", err), http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
-
 	if contentLength, ok := resp.Header["Content-Length"]; ok {
 		if size, err := strconv.Atoi(contentLength[0]); err == nil && size > sizeLimit {
 			finalURL := resp.Request.URL.String()
@@ -101,17 +90,12 @@ func proxy(c *gin.Context, u string) {
 			return
 		}
 	}
-
-	resp.Header.Del("Content-Security-Policy")
-	resp.Header.Del("Referrer-Policy")
-	resp.Header.Del("Strict-Transport-Security")
-
+	c.Header("Cache-Control", "public, max-age=604800")
 	for key, values := range resp.Header {
 		for _, value := range values {
 			c.Header(key, value)
 		}
 	}
-
 	c.Writer.WriteHeader(resp.StatusCode)
 	if _, err := io.Copy(c.Writer, resp.Body); err != nil {
 		return
